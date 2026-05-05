1617a493e885e004b989699dcda9b39354e5d4fd
danis <danis@lenova.com>
2026-05-05 22:20:32 +0300

    chore: Начала проекта

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+import datetime as dt
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+
+START_URL = "https://spimex.com/markets/oil_products/trades/results/"
+START_FROM = dt.datetime(2023, 1, 1).date()
+DEST_FOLDER = "data"
+MAX_DOWNLOAD_WORKERS = 3
+MAX_PROCESS_WORKERS = 4

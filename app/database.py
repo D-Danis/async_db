d7fcbb8b5d7fd4a933e2b83791012d6e2f83cd9d
danis <danis@lenova.com>
2026-05-05 22:26:15 +0300

    feat: Подключение к БД

---
+++ app/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import DeclarativeBase
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# Создаём асинхронный движок
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,           
+    pool_size=10,          
+    max_overflow=5,       
+)
+
+async_session_factory = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,  
+)
+
+class Base(DeclarativeBase):
+    pass
+
